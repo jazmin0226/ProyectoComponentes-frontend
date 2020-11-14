ce557ffc23ffc6e9f44649053cf21feec849de95
jazmin0226 <jariass@ucenfotec.ac.cr>
2020-11-14 00:43:45 -0600

    update sin llamar al servicio

--- js/list.controller.js
+++ js/list.controller.js
@@ -2,19 +2,26 @@ class listController {
   service = new service;
   table = $('#tableId')[0];
   bodyTable = $('#bodyTable');
+  loadingElement = $('#loading');
 
   fillTable(arrayData) {
     for (const currentData of arrayData) {
       let elementTable = '<tr> <td>';
-      
+      let button;
       for (const key in currentData) {
         if (currentData.hasOwnProperty(key)) {
           const element = currentData[key];
-          elementTable +=`${element}</td><td>` 
+          elementTable += `${element}</td><td>`;
+          button = `<button type="button" class="btn btn-info editButton" data-id="${currentData.id}" data-toggle="modal" data-target="#modalId">Editar</button>`;
         }
       }
-      elementTable += '</td></tr>';
+      elementTable += `${button}</td></tr>`;
       this.bodyTable.append(elementTable);
     }
   }
+  
+  hideLoading() {
+    this.loadingElement.hide();
+  }
+  
 }
--- js/product/retrieve.controller.js
+++ js/product/retrieve.controller.js
@@ -8,7 +8,30 @@ class retrieveController extends listController{
     this.service.getData(this.entity).then((response) => {
       this.currentProducts = response.newData;
       this.fillTable(this.currentProducts);
+      this.addEditEvent();
+      this.hideLoading();
     });
+  }
+
+  addEditEvent() {
+    const buttons = $('.editButton');
+    for (const button of buttons) {
+      $(button).on('click', () => {
+          this.showModalEdit(button.dataset.id);
+      });
+    }
+  }
+
+  showModalEdit(elementId) {
+    const index = Number(elementId);
+    const currentProduct = this.currentProducts[index - 1];
+    const name = $('#name')[0];
+    const color = $('#color')[0];
+    const description = $('#description')[0];
+
+    name.value = currentProduct.name;
+    color.value = currentProduct.color;
+    description.value = currentProduct.description;
   }
 }
 
