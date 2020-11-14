9e0dd53e1be885eae833508370b0c995bb3f3da4
jazmin0226 <jariass@ucenfotec.ac.cr>
2020-11-14 10:19:55 -0600

    Update register.controller.js

--- js/product/register.controller.js
+++ js/product/register.controller.js
@@ -5,7 +5,7 @@ class registerController extends formController {
   registerData() {
     if (this.validateFormData()) {
       this.service.registerData(this.entity, this.getFormData()).then((response) => {
-        this.showSuccess("Registro correcto", "Se ha registrado correctamente.");
+        this.showSuccess("", "Se ha registrado correctamente.");
       });
     } else {
       this.showAlert();
