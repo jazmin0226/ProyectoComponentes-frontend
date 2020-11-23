346d17b826ed60113516d817f20d344bf0c65b21
Valeria Edwards <valeriaedwards17@gmail.com>
2020-11-23 12:19:01 -0600

    Manda los datos de la lista a la vetana modal de update

--- js/user/retrieve.controller.js
+++ js/user/retrieve.controller.js
@@ -2,6 +2,7 @@ class retrieveController extends listController{
 
   entity = 'users'; //Entidad para el SERVICIO
   currentUsers;
+  currentIndexUser;
   
   constructor (){
     super();
@@ -24,6 +25,27 @@ class retrieveController extends listController{
         this.showModalEdit(buttons.dataset.id);
       });
     }
+  }
+
+  showModalEdit(elementId){
+    const index = Number (elementId);
+    const currentUser = this.currentUsers[index];
+    const type = $('#type')[0];
+    const id = $('#id')[0];
+    const name= $('#name')[0];
+    const phone = $('#phone')[0];
+    const mail = $('#mail')[0];
+    const address = $('#address')[0];
+
+    this.currentIndexUser = index;
+    type.value = currentUser.type;
+    id.value = currentUser.id;
+    name.value = currentUser.name;
+    phone.value = currentUser.phone;
+    mail.value = currentUser.mail;
+    address.value = currentUser.address;
+
+
   }
 }
   
