0b8107ba58b6b781f8656d91600bc5d1a74feb2e
Valeria Edwards <valeriaedwards17@gmail.com>
2020-11-23 11:43:45 -0600

    Hice modal de update usuarios y acomode el metodo de fillData

--- js/user/retrieve.controller.js
+++ js/user/retrieve.controller.js
@@ -1,15 +1,30 @@
 class retrieveController extends listController{
 
-    entity = 'users'; //Entidad para el SERVICIO
-    currentUsers;
+  entity = 'users'; //Entidad para el SERVICIO
+  currentUsers;
   
-    constructor (){
-      super();
-      this.service.getData(this.entity).then((response) => {
-        this.currentUsers = response.newData;
-        this.fillTable(this.currentUsers);
+  constructor (){
+    super();
+    this.fillData();
+
+  }
+
+  fillData(){
+    this.service.getData(this.entity).then((response) => {
+      this.currentUsers = response.newData;
+      this.fillTable(this.currentUsers);
+      this.addEditEvent();
+    });
+  }
+
+  addEditEvent(){
+    const button = $('.editButton');
+    for(const buttons of button){
+      $(buttons).on('click',()=>{
+        this.showModalEdit(buttons.dataset.id);
       });
     }
+  }
 }
   
 const controller = new retrieveController();
